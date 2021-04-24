b89e2e6c196d073e1b2a0202226c7e3c482476ec
Jahnavi Gupta <34552448+janvig@users.noreply.github.com>
2021-04-24 22:24:22 +0530

    Add linter (#12)
    * add linter
    
    * add eslint dependency, scripts
    
    * lint code
    
    * fix linter for Jest test files
    
    * update github action workflow

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  "rules": {
+    "semi": ["error", "never"],
+    "array-bracket-spacing": [
+      "error",
+      "never"
+    ],
+    "no-trailing-spaces": "error",
+    "comma-spacing": [
+      "error",
+      {
+        "before": false,
+        "after": true
+      }
+    ],
+    "indent": [
+      "error",
+      2
+    ],
+    "key-spacing": [
+      "error",
+      {
+        "beforeColon": false,
+        "afterColon": true
+      }
+    ],
+    "quotes": ["error", "double"],
+    "space-before-function-paren": ["error", "never"],
+    "space-before-blocks": ["error", "never"],
+    "prefer-promise-reject-errors": "off"
+  },
+  "env": {
+    "node": true,
+    "es2021": true,
+    "browser": true,
+  },
+  "extends": "eslint:recommended",
+  "overrides": [
+    {
+      "files": [
+        "**/*.test.js",
+      ],
+      "env": {
+        "jest": true
+      }
+    }
+  ]
+}
